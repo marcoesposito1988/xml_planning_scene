1d7795ec65c27aff0a7f86731ffba714ca21914e
Marco Esposito <marco_esposito@mytum.de>
2014-09-12 13:42:11 +0200

    Added file and frequency parameters
    The scene can be published once or at a user-defined frequency.
    The file can be provided as argument to the launch file as well.

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -17,6 +17,7 @@ rospy.init_node('PlanningScenePublisher')
 
 parser = argparse.ArgumentParser(description="Publishes the MoveIt! planning scene contained in an XML file")
 parser.add_argument('file',help='scene file path')
+parser.add_argument('frequency',help='update frequency in Hz (0 for no updating)',default=0,type=int)
 args = parser.parse_args(args=rospy.myargv(sys.argv)[1:])
     
 s = XMLSceneParser.parse_scene_file(args.file)
@@ -24,14 +25,20 @@ edit_time = os.stat(args.file).st_mtime
 
 p = rospy.Publisher('/planning_scene_world',PlanningSceneWorld)
 
-r = rospy.Rate(1) # 1hz
-try:
-    while not rospy.is_shutdown():
-        if os.stat(args.file).st_mtime != edit_time:
-            s = XMLSceneParser.parse_scene_file(args.file)
-            edit_time = os.stat(args.file).st_mtime
-        p.publish(s)
-        r.sleep()
-except rospy.ROSInterruptException: pass
-    
+if args.frequency == 0:
+    rospy.loginfo('publishing once scene file: '+args.file)
+    s = XMLSceneParser.parse_scene_file(args.file)
+    p.publish(s)
+else:
+    rospy.loginfo('publishing scene file: '+args.file+' with frequency: '+str(args.frequency))
+    r = rospy.Rate(args.frequency)
+    try:
+        while not rospy.is_shutdown():
+            if os.stat(args.file).st_mtime != edit_time:
+                s = XMLSceneParser.parse_scene_file(args.file)
+                edit_time = os.stat(args.file).st_mtime
+            p.publish(s)
+            r.sleep()
+    except rospy.ROSInterruptException: pass
+
     
